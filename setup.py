0fb9b9c38daa98681cfe6c66667613bf491b0285
supakeen <cmdr@supakeen.com>
2019-02-22 08:30:50 +0100

    0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pinnwand",
-    version="0.0.1",
+    version="0.0.2",
     description="Straightforward pastebin software.",
     url="https://github.com/supakeen/pinnwand",
     author="supakeen",
