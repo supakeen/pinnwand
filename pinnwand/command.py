1b3b7b1572eb9a87104f82f0b35207dcef148ead
supakeen <cmdr@supakeen.com>
2019-06-22 09:14:34 +0000

    Create click group and commands to make the application runnable.

--- pinnwand/command.py
+++ pinnwand/command.py
@@ -8,28 +8,35 @@ import logging
 
 from datetime import datetime, timedelta
 
+import tornado.ioloop
+
 from pinnwand.database import Base, engine, session, Paste
+from pinnwand.http import application
 
 
 log = logging.getLogger(__name__)
 
 
-@click.command()
+@click.group()
 def main() -> None:
     """Pinnwand pastebin software."""
-    pass
+    print("Running main")
 
 
+@main.command()
 def init() -> None:
     """Create pinnwand's database models."""
     Base.metadata.create_all(engine)
 
 
+@main.command()
 def http() -> None:
     """Run pinnwand's HTTP server."""
-    app.run("0.0.0.0", 8000)
+    application.listen(8000)
+    tornado.ioloop.IOLoop.current().start()
 
 
+@main.command()
 def add() -> None:
     """Add a paste to pinnwand's database from stdin."""
     paste = Paste(sys.stdin.read(), lexer="html", expiry=timedelta(days=1))
@@ -38,6 +45,7 @@ def add() -> None:
     session.commit()
 
 
+@main.command()
 def delete() -> None:
     """Delete a paste from pinnwand's database."""
     paste = session.query(Paste).filter(Paste.id == int(args[1])).first()
@@ -46,6 +54,7 @@ def delete() -> None:
     session.commit()
 
 
+@main.command()
 def reap() -> None:
     """Delete all pastes that are past their expiry date in pinnwand's
        database."""
