fcc83ed38cce099f71eb5037a6af59bb993a521c
supakeen <cmdr@supakeen.com>
2019-06-29 18:20:23 +0000

    Actually set correct names.

--- pinnwand/http.py
+++ pinnwand/http.py
@@ -268,9 +268,9 @@ def make_application() -> tornado.web.Application:
             (r"/about", AboutPage),
             (r"/removal", RemovalPage),
             (r"/expiry", ExpiryPage),
-            (r"/api/new", APINew),
-            (r"/api/remove", APIRemove),
-            (r"/api/show", APIShow),
+            (r"/json/new", APINew),
+            (r"/json/remove", APIRemove),
+            (r"/json/show", APIShow),
             (
                 r"/static/(.*)",
                 tornado.web.StaticFileHandler,
