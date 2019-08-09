3b9196d4ab180bf08fce31abe80c88d2924e7133
supakeen <cmdr@supakeen.com>
2019-08-09 07:38:31 +0000

    Start using versioneer as requested in #16.

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup, find_packages
 
+import versioneer
+
+
 setup(
     name="pinnwand",
-    version="0.0.2",
+    version=versioneer.get_version(),  # type: ignore
+    cmdclass=versioneer.get_cmdclass(),  # type: ignore
     description="Straightforward pastebin software.",
     url="https://github.com/supakeen/pinnwand",
     author="supakeen",
     author_email="cmdr@supakeen.com",
     packages=find_packages(),
     setup_requires=["pytest-runner", "pytest-cov"],
-    install_requires=[
-        "click",
-        "tornado",
-        "pygments",
-        "sqlalchemy",
-    ],
+    install_requires=["click", "tornado", "pygments", "sqlalchemy"],
     tests_require=["pytest", "pytest-cov"],
     entry_points={"console_scripts": ["pinnwand=pinnwand.command:main"]},
     extras_require={
