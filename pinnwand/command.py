cafb05a07100ed9be0fcc15098dd08c8bec2c437
supakeen <cmdr@supakeen.com>
2020-02-26 20:53:23 +0000

    Change up import ordering to close #46.
    This is a workaround for storing the configuration in global state.

--- pinnwand/command.py
+++ pinnwand/command.py
@@ -13,12 +13,6 @@ import click
 
 import tornado.ioloop
 
-from pinnwand import utility
-from pinnwand import database
-from pinnwand import configuration
-
-from pinnwand.http import make_application
-
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -28,6 +22,8 @@ logging.basicConfig(level=logging.DEBUG)
 @click.option("--configuration-path", default=None, help="Configuration file.")
 def main(configuration_path: Optional[str]) -> None:
     """Pinnwand pastebin software."""
+    from pinnwand import configuration
+
     if configuration_path:
         import toml
 
@@ -37,12 +33,16 @@ def main(configuration_path: Optional[str]) -> None:
             for key, value in configuration_file.items():
                 setattr(configuration, key, value)
 
+    from pinnwand import database
+
+    database.Base.metadata.create_all(database._engine)
+
 
 @main.command()
 @click.option("--port", default=8000, help="Port to listen to.")
 def http(port: int) -> None:
     """Run pinnwand's HTTP server."""
-    database.Base.metadata.create_all(database._engine)
+    from pinnwand.http import make_application
 
     application = make_application()
     application.listen(port, xheaders=True)
@@ -53,6 +53,9 @@ def http(port: int) -> None:
 @click.option("--lexer", default="text", help="Lexer to use.")
 def add(lexer: str) -> None:
     """Add a paste to pinnwand's database from stdin."""
+    from pinnwand import database
+    from pinnwand import utility
+
     if lexer not in utility.list_languages():
         log.error("add: unknown lexer")
         return
@@ -72,6 +75,8 @@ def add(lexer: str) -> None:
 @click.option("--paste", help="database.Paste identifier.", required=True)
 def delete(paste: str) -> None:
     """Delete a paste from pinnwand's database."""
+    from pinnwand import database
+
     with database.session() as session:
         paste_object = (
             session.query(database.Paste)
@@ -93,6 +98,8 @@ def delete(paste: str) -> None:
 def reap() -> None:
     """Delete all pastes that are past their expiry date in pinnwand's
        database."""
+    from pinnwand import database
+
     with database.session() as session:
         pastes = (
             session.query(database.Paste)
