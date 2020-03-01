4a4924ed16eb276606c0aba6d984abf58b974eba
supakeen <cmdr@supakeen.com>
2020-03-01 15:08:35 +0000

    Does this fix autodoc?

--- doc/conf.py
+++ doc/conf.py
@@ -12,12 +12,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 
-# sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath("../"))
 
-# import pinnwand  # noqa: E402
+import pinnwand  # noqa
 
 
 # -- Project information -----------------------------------------------------
