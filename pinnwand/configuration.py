404fca6880890ffd38f883d2960116c520781d5c
supakeen <cmdr@supakeen.com>
2020-03-05 15:07:01 +0000

    Add preferred_lexers configuration as per #60.

--- pinnwand/configuration.py
+++ pinnwand/configuration.py
@@ -3,3 +3,4 @@ paste_size = 256 * 1024  # in bytes
 footer = 'View <a href="//github.com/supakeen/pinnwand" target="_BLANK">source code</a>, the <a href="/removal">removal</a> or <a href="/expiry">expiry</a> stories, or read the <a href="/about">about</a> page.'
 page_path = None
 page_list = ["about", "removal", "expiry"]
+preferred_lexers = []  # type: ignore
