5aef73dbcabc3d560cf2df0d6d7033e703b0ab3c
supakeen <cmdr@supakeen.com>
2020-03-24 21:41:42 +0000

    Ignore doc/conf.py instead of using noqa.

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../"))
 
-import pinnwand  # noqa
+import pinnwand
 
 
 # -- Project information -----------------------------------------------------
