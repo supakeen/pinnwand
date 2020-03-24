e0592563a35e7b0c7f1ed8d3fe32c763debab965
supakeen <cmdr@supakeen.com>
2020-03-24 21:44:41 +0000

    Reformat and update pre-commit to exclude tests.

--- pinnwand/http.py
+++ pinnwand/http.py
@@ -45,8 +45,16 @@ def make_application() -> tornado.web.Application:
 
     if configuration.logo_path:
         pages += [
-            (r"/static/logo.png", handler.website.Logo, {"path": configuration.logo_path}),
-            (r"/static/favicon.png", handler.website.Logo, {"path": configuration.logo_path}),
+            (
+                r"/static/logo.png",
+                handler.website.Logo,
+                {"path": configuration.logo_path},
+            ),
+            (
+                r"/static/favicon.png",
+                handler.website.Logo,
+                {"path": configuration.logo_path},
+            ),
         ]
 
     pages += [
