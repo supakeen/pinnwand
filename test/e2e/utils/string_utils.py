ca4f2f0b9bf99a5d35095036a8cd2d5516187844
Yuliya <yu@novikova.nl>
2023-09-20 12:01:27 +0200

    test: added tests for filenames

--- test/e2e/utils/string_utils.py
+++ test/e2e/utils/string_utils.py
@@ -16,5 +16,9 @@ def random_string(size=1000) -> str:
     )
 
 
+def random_letter_string(size=10) -> str:
+    return "".join([random.choice(string.ascii_letters) for i in range(size)])
+
+
 def convert_new_lines(text):
     return text.replace("\n", "\r\n")
