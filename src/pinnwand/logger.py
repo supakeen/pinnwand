e76c7f221018a1b42924f293ee2b0c506706bf54
shtlrs <amrbellalouna@gmail.com>
2023-12-06 16:17:19 +0100

    add a logger module that'll boostrap loggers on demand.

---
+++ src/pinnwand/logger.py
@@ -0,0 +1,13 @@
+import logging
+
+LoggerClass = logging.getLoggerClass()
+
+
+def get_logger(name: str) -> LoggerClass:
+    logger = logging.getLogger(name)
+
+    for handler in logger.handlers:
+        formatter = logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(message)s")
+        handler.setFormatter(formatter)
+
+    return logger
