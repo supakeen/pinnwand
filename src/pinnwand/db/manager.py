8a8058a8f4e39937a58dfc4a273650d95cca7953
shtlrs <amrbellalouna@gmail.com>
2024-01-13 22:41:40 +0100

    delegate database related resource management to DatabaseManager
    This ensure that we don't have global resource variables available all across modules.
    
    The delegation will help contain the instance better, and operate more granulay on it during tests.

---
+++ src/pinnwand/db/manager.py
@@ -0,0 +1,40 @@
+import contextlib
+
+from sqlalchemy import create_engine, Engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+
+from pinnwand import configuration
+
+
+class DatabaseManager:
+    """An entity responsible for managing database-related resources."""
+
+    _engine: Engine = None
+    _session_maker = None
+
+    @classmethod
+    def get_engine(cls):
+        """Return an engine for the currently configured connection string."""
+
+        if not cls._engine:
+            cls._engine = create_engine(configuration.database_uri)
+
+        return cls._engine
+
+    @classmethod
+    @contextlib.contextmanager
+    def get_session(cls) -> Session:
+        """Create a self-disposable database session."""
+        if not cls._session_maker:
+            cls._session_maker = sessionmaker(bind=cls.get_engine())
+
+        new_session = cls._session_maker()
+        try:
+            yield new_session
+        except Exception:
+            new_session.rollback()
+            raise
+        finally:
+            new_session.close()
