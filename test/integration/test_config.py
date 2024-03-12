97525e394f263dd6aaa56da6ba090e262d536631
shtlrs <amrbellalouna@gmail.com>
2024-03-12 15:46:19 +0100

    add tests for loading config from .env files

--- test/integration/test_config.py
+++ test/integration/test_config.py
@@ -1,12 +1,38 @@
+import os
+
 from pinnwand.configuration import Configuration
 import tempfile
 import toml
+import pytest
 
 
-def test_load_environment_config(monkeypatch):
+@pytest.fixture(scope="function")
+def temp_dotenv_file():
+
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as file:
+        path = file.name
+
+    yield path
+    os.remove(path)
+
+
+def test_load_environment_config(temp_dotenv_file, monkeypatch):
     monkeypatch.setenv("PINNWAND_SPAMSCORE", "29")
+    with open(temp_dotenv_file, "w+") as file:
+        file.write("PINNWAND_PASTE_SIZE=524288")
     config = Configuration()
-    config.load_environment()
+    config.load_environment(dotenv_file_path=temp_dotenv_file)
+    assert config.spamscore == 29
+    assert config.paste_size == 524288
+
+
+def test_env_variables_precedence(monkeypatch, temp_dotenv_file):
+    monkeypatch.setenv("PINNWAND_SPAMSCORE", "29")
+    with open(temp_dotenv_file, "w+") as file:
+        file.write("PINNWAND_SPAMSCORE=31")
+
+    config = Configuration()
+    config.load_environment(dotenv_file_path=temp_dotenv_file)
     assert config.spamscore == 29
 
 
