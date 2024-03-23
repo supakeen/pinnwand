07d933ec795fa5e5478db862e47918ef82ac9d18
shtlrs <amrbellalouna@gmail.com>
2024-03-23 23:32:24 +0100

    expose new_context & new_page as context managers from BrowserManager
    This will ensure that resources are opened/closed properly

--- test/e2e/testscenarios/test_create_paste.py
+++ test/e2e/testscenarios/test_create_paste.py
@@ -1,15 +1,14 @@
 from test.e2e.conftest import create_paste_page
 from test.e2e.pageobjects.create_paste_page import CreatePastePage
 from test.e2e.pageobjects.view_paste_page import ViewPastePage
-from test.e2e.playwright.browser_manager import BrowserManager
 
 import pytest
-from playwright.sync_api import Page, Playwright
+from playwright.sync_api import Page
 
 
 @pytest.mark.e2e
 def test_create_single_paste(
-    page: Page, playwright: Playwright, create_paste_page: CreatePastePage
+    page: Page, create_paste_page: CreatePastePage
 ):
     create_paste_page.should_have_title("Create new paste")
 
@@ -21,12 +20,12 @@ def test_create_single_paste(
     view_paste_page.should_have_pasted_text(pasted_text)
 
     paste_url = view_paste_page.current_url()
-    reopen_created_paste(playwright, paste_url)
+    reopen_created_paste(page, paste_url)
 
 
 @pytest.mark.e2e
 def test_create_multi_paste(
-    page: Page, playwright: Playwright, create_paste_page: CreatePastePage
+    page: Page, create_paste_page: CreatePastePage
 ):
     first_pasted_text = create_paste_page.paste_random_text(paste_number=0)
     create_paste_page.click_add_another_file_button()
@@ -45,11 +44,10 @@ def test_create_multi_paste(
     view_paste_page.should_have_pasted_text(third_pasted_text, paste_number=2)
 
     paste_url = view_paste_page.current_url()
-    reopen_created_paste(playwright, paste_url)
+    reopen_created_paste(page, paste_url)
 
 
-def reopen_created_paste(playwright, paste_url):
-    new_page = BrowserManager(playwright).create_new_context()
-    new_view_paste_page = ViewPastePage(new_page)
+def reopen_created_paste(page: Page, paste_url: str):
+    new_view_paste_page = ViewPastePage(page)
     new_view_paste_page.open(paste_url)
     new_view_paste_page.should_be_opened()
